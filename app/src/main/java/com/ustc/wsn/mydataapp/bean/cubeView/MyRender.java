b91e0a3a258d2342e8fdaa51b6d56b25c9679f62
halostorm <2524416894@qq.com>
2018-03-27 18:47:25 +0800

    create PhoneGlModel

--- app/src/main/java/com/ustc/wsn/mydataapp/bean/cubeView/MyRender.java
+++ app/src/main/java/com/ustc/wsn/mydataapp/bean/cubeView/MyRender.java
@@ -4,6 +4,12 @@ package com.ustc.wsn.mydataapp.bean.cubeView;
  * Created by halo on 2018/3/26.
  */
 
+import android.opengl.GLSurfaceView.Renderer;
+import android.util.Log;
+
+import com.ustc.wsn.mydataapp.bean.PhoneState;
+import com.ustc.wsn.mydataapp.bean.math.myMath;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.IntBuffer;
@@ -11,59 +17,25 @@ import java.nio.IntBuffer;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import android.annotation.SuppressLint;
-import android.app.Service;
-import android.content.Context;
-import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
-import android.opengl.GLSurfaceView.Renderer;
-import android.os.IBinder;
-import android.util.Log;
-import android.view.Gravity;
-import android.widget.Toast;
-
-import com.ustc.wsn.mydataapp.Listenter.TrackSensorListener;
-import com.ustc.wsn.mydataapp.bean.PhoneState;
-import com.ustc.wsn.mydataapp.bean.StoreData;
-import com.ustc.wsn.mydataapp.bean.math.myMath;
+import static javax.microedition.khronos.opengles.GL10.GL_VERTEX_ARRAY;
 
 public class MyRender implements Renderer {
     private final String TAG = MyRender.class.toString();
 
     private volatile float[] eulerRead = {0f, 0f, 0f};
     private volatile float[] Euler = {0f, 0f, 0f};
-    int one = 0x10000*2;
-    private IntBuffer colorBuffer;
-    private int[] colors = new int[]{
+    private PhoneGlModel phone;
 
-            one / 4, one, 0, one, one / 4, one, 0, one, one / 4, one, 0, one, one / 4, one, 0, one,
-
-            one, one / 4, 0, one, one, one / 4, 0, one, one, one / 4, 0, one, one, one / 4, 0, one, one, one, 0, one, one, one, 0, one, one, one, 0, one, one, one, 0, one, one, 0, 0, one, one, 0, 0, one, one, 0, 0, one, one, 0, 0, one,
-
-            0, 0, one, one, 0, 0, one, one, 0, 0, one, one, 0, 0, one, one,
-
-            one, 0, one, one, one, 0, one, one, one, 0, one, one, one, 0, one, one,};
-
-    private IntBuffer quaterBuffer;
-    private int[] quaterVertices = new int[]{one/2, one, -one/10, -one/2, one, -one/10, one/2, one, one/10, -one/2, one, one/10,
-
-            one/2, -one, one/10, -one/2, -one, one/10, one/2, -one, -one/10, -one/2, -one, -one/10,
-
-            one/2, one, one/10, -one/2, one, one/10, one/2, -one, one/10, -one/2, -one, one/10,
-
-            one/2, -one, -one/10, -one/2, -one, -one/10, one/2, one, -one/10, -one/2, one, -one/10,
-
-            -one/2, one, one/10, -one/2, one, -one/10, -one/2, -one, one/10, -one/2, -one, -one/10,
-
-            one/2, one, -one/10, one/2, one, one/10, one/2, -one, -one/10, one/2, -one, one/10};
+    public MyRender(){
+        phone = new PhoneGlModel();
+    }
 
     @Override
     public void onDrawFrame(GL10 gl) {
 
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 
-        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL_VERTEX_ARRAY);
         gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
         gl.glLoadIdentity();
 
@@ -74,8 +46,8 @@ public class MyRender implements Renderer {
         gl.glRotatef(Euler[1], 1.0f, 0.0f, 0.0f);
         gl.glRotatef(Euler[0], 0.0f, 1.0f, 0.0f);
 
-        gl.glColorPointer(4, GL10.GL_FIXED, 0, colorBuffer);
-        gl.glVertexPointer(3, GL10.GL_FIXED, 0, quaterBuffer);
+        gl.glColorPointer(4, GL10.GL_FIXED, 0, phone.colorBuffer);
+        gl.glVertexPointer(3, GL10.GL_FIXED, 0, phone.quaterBuffer);
 
 
         for (int i = 0; i < 6; i++) {
@@ -83,7 +55,7 @@ public class MyRender implements Renderer {
         }
 
         gl.glFinish();
-        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glDisableClientState(GL_VERTEX_ARRAY);
         gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
 
         Log.d(TAG, "cube view");
@@ -134,16 +106,16 @@ public class MyRender implements Renderer {
 
         gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
 
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(colors.length * 4);
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(phone.colors.length * 4);
         byteBuffer.order(ByteOrder.nativeOrder());
-        colorBuffer = byteBuffer.asIntBuffer();
-        colorBuffer.put(colors);
-        colorBuffer.position(0);
+        phone.colorBuffer = byteBuffer.asIntBuffer();
+        phone.colorBuffer.put(phone.colors);
+        phone.colorBuffer.position(0);
 
-        byteBuffer = ByteBuffer.allocateDirect(quaterVertices.length * 4);
+        byteBuffer = ByteBuffer.allocateDirect(phone.quaterVertices.length * 4);
         byteBuffer.order(ByteOrder.nativeOrder());
-        quaterBuffer = byteBuffer.asIntBuffer();
-        quaterBuffer.put(quaterVertices);
-        quaterBuffer.position(0);
+        phone.quaterBuffer = byteBuffer.asIntBuffer();
+        phone.quaterBuffer.put(phone.quaterVertices);
+        phone.quaterBuffer.position(0);
     }
 }
